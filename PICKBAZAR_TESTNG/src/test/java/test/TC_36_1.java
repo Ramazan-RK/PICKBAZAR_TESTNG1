138fa977827e757a9626143d50621a252eda97bd
ramazan-rk <rcarol3503@gmail.com>
2023-01-16 20:21:52 +0300

    16.01.2023

---
+++ PICKBAZAR_TESTNG/src/test/java/test/TC_36_1.java
@@ -0,0 +1,32 @@
+package test;
+
+import com.aventstack.extentreports.ExtentReports;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class TC_36_1 {
+
+
+
+    WebDriver driver;
+    private Object extentTest;
+    private ExtentReports extentReports;
+
+    @Test
+    public void test01(){
+
+
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+        this.extentTest = this.extentReports.createTest("PICKBAZAR");
+
+
+
+
+
+
+    }
+
+}
